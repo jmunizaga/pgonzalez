bf6a5380a6574e383c1e41e3590f5e9edd4d65f9
jmunizaga@bitbucket <jaime.munizaga.l@gmail.com>
2017-07-08 01:56:03 -0400

    FV 01

--- src/java/servlets/AdminAtenciones.java
+++ src/java/servlets/AdminAtenciones.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servlets;
 
 import java.io.IOException;
@@ -16,10 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import persistencia.Atencion;
-import persistencia.Cliente;
 import persistencia.Ficha;
-import persistencia.Mascota;
-import persistencia.Raza;
 import persistencia.Veterinario;
 
 public class AdminAtenciones extends HttpServlet {
